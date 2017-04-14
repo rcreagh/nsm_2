df68f8380785be690059def521620fc349c5f076
maxwbuckley <mawbuckley@gmail.com>
2017-04-14 18:38:43 +0100

    Made a lot of changes. Most interesting is the modify_graph argument to the virus simulation

--- pq_simulation.py
+++ pq_simulation.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """Priority Queue based simulation. The priority queue stores the events that
-   are due to occur."""
-import queue
-import networkx
-import random
-import numpy
-import enum
+   are due to occur. Creates a virus simulation modeling the spread through the
+   population."""
+# Keep sorted alphabetically.
 import collections
+import enum
+import networkx
+import numpy
 import pandas
+import queue
+import random
 
 
 # For the random graph
@@ -16,10 +18,10 @@ M = 30
 SEED = 13378
 
 
-Result = collections.namedtuple("SimulationResults", (
-    "MortalityRate", "TimeToDeath", "TimeToRecovery", "EmailTime",
+SimulationResult = collections.namedtuple("SimulationResult", (
+    "MortalityRate", "TimeToDeath", "TimeToRecovery", "CommunicationTime",
     "ResistantProportion", "Time", "Infected", "Resistant", "Recovered",
-    "Dead", "Emails"))
+    "Dead", "Communication"))
 
 
 class Status(enum.Enum):
@@ -29,44 +31,51 @@ class Status(enum.Enum):
   DEAD = 4
 
 
+class NoNodesLeftAliveException(Exception):
+  """Exception raised when there are no nodes left in the passed in graph."""
+  pass
+
+
 # Only need to inherit from object in Python2.
 class VirusSimulation:
-  def __init__(self, n_nodes, m_edges, seed, mortality_rate=.2,
-               time_to_death=25, time_to_recovery=25, email_time=1,
-               resistant_proportion=.1):
-    self.graph = networkx.barabasi_albert_graph(n_nodes, m_edges, seed=seed)
+  def __init__(self, graph, mortality_rate=.2, time_to_death=25,
+               time_to_recovery=25, communication_time=1,
+               resistant_proportion=.1, max_iterations=100000,
+               modify_graph=False):
+    self.graph = graph
+    # Stores infection status of the nodes. Stored here to prevent modification
+    # of passed in graph. Stops us from having to reinitalize or copy graphs.
+    self.node_status = {}
     self.pq = queue.PriorityQueue()
     self.mortality_rate = mortality_rate
     self.time_to_death = time_to_death
     self.time_to_recovery = time_to_recovery
-    self.email_time = email_time
+    self.communication_time = communication_time
     self.resistant_proportion = resistant_proportion
+    self.max_iterations = max_iterations
+    self.modify_graph = modify_graph
     self.time = 0
     # Stats recording
     self.resistant = 0
     self.deaths = 0
     self.recoveries = 0
     self.infections = 0
-    self.emails = 0
-    # We infect the first node, node 0.
-    self.infect(0, True)
-
-  def input_parameters(self):
-    return [self.mortality_rate,
-            self.time_to_death,
-            self.time_to_recovery,
-            self.email_time,
-            self.resistant_proportion]
+    self.communications = 0
+    if len(self.graph.nodes()) == 0:
+      raise NoNodesLeftAliveException()
+    # We infect the first node.
+    self.infect(random.choice(self.graph.nodes())
+, True)
 
   def next_event(self, exponential_mean):
     return self.time + numpy.random.exponential(exponential_mean)
 
   def infect(self, node, patient_zero=False):
     if not patient_zero and random.random() < self.resistant_proportion:
-      self.graph.node[node]["status"] = Status.RESISTANT
+      self.node_status[node] = Status.RESISTANT
       self.resistant += 1
     else:
-      self.graph.node[node]["status"] = Status.INFECTED
+      self.node_status[node] = Status.INFECTED
       # Enqueue communication
       if random.random() < self.mortality_rate:
         # Node will die
@@ -77,40 +86,51 @@ class VirusSimulation:
         recovery_time = self.next_event(self.time_to_recovery)
         self.pq.put((recovery_time, self.recover, node))
       # Regardless we need to enque communications
-      email_time = self.next_event(self.email_time)
-      self.pq.put((email_time, self.mail, node))
+      communication_time = self.next_event(self.communication_time)
+      self.pq.put((communication_time, self.communicate, node))
       self.infections += 1
 
-  def mail(self, node):
-    if self.graph.node[node]["status"] == Status.INFECTED:
-      # Select a random neighbor and infect them if possible.
-      target_node = random.choice(self.graph.neighbors(node))
-      if self.graph.node[target_node].get("status") is None:
-        self.infect(target_node)
+  def communicate(self, node):
+    if self.node_status[node] == Status.INFECTED:
+      # Select a random neighbor if you have any and infect them if possible.
+      if len(self.graph.neighbors(node)) > 0:
+        target_node = random.choice(self.graph.neighbors(node))
+        if self.node_status.get(target_node) is None:
+          self.infect(target_node)
       # Enqueue another communication from this infected node.
-      email_time = self.next_event(self.email_time)
-      self.pq.put((email_time, self.mail, node))
-      self.emails += 1
+      communication_time = self.next_event(self.communication_time)
+      self.pq.put((communication_time, self.communicate, node))
+      self.communications += 1
 
   def recover(self, node):
-    if self.graph.node[node]["status"] == Status.INFECTED:
-      self.graph.node[node]["status"] = Status.RECOVERED
+    if self.node_status[node] == Status.INFECTED:
+      self.node_status[node] = Status.RECOVERED
       self.recoveries += 1
 
   def death(self, node):
-    if self.graph.node[node]["status"] == Status.INFECTED:
-      self.graph.node[node]["status"] = Status.DEAD
+    if self.node_status[node] == Status.INFECTED:
+      self.node_status[node] = Status.DEAD
       self.deaths += 1
+      if self.modify_graph:
+        self.graph.remove_node(node)
 
-  def run_virus(self, max_iterations=100000):
-    while self.time < max_iterations and not self.pq.empty():
+  def input_parameters(self):
+    return [self.mortality_rate,
+            self.time_to_death,
+            self.time_to_recovery,
+            self.communication_time,
+            self.resistant_proportion]
+
+  def run_virus(self):
+    while self.time < self.max_iterations and not self.pq.empty():
       timestep, function, node = self.pq.get()
       self.time = timestep
       function(node)
     print("Final time = %s" % (self.time))
-    print("%d infections, %d resistant, %d recovered, %d dead %d emails" % (
+    print(("%d infections, %d resistant, %d recovered, %d dead "
+           "%d communications") % (
         self.infections, self.resistant, self.recoveries, self.deaths,
-        self.emails))
+        self.communications))
     # This returns all the data regarding this simulation.
     return (*self.input_parameters(),
             self.time,
@@ -118,13 +138,18 @@ class VirusSimulation:
             self.resistant,
             self.recoveries,
             self.deaths,
-            self.emails)
+            self.communications)
 
 
 if __name__ == "__main__":
+  graph = networkx.barabasi_albert_graph(N, M, seed=SEED)
   results = []
   for i in range(30):
-    virus_simulation = VirusSimulation(N, M, SEED)
-    results.append(Result(*virus_simulation.run_virus()))
+    try:
+      virus_simulation = VirusSimulation(graph, modify_graph=True)
+      results.append(SimulationResult(*virus_simulation.run_virus()))
+    except NoNodesLeftAliveException:
+      # If the graph nodes are empty from delete in modify_graph mode.
+      continue
   dataframe = pandas.DataFrame(results)
   print(dataframe)
